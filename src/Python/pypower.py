77a2ac6fd7483e6718942efc6f7b3694f78a6fea
jomariapc <jomariapc@gmail.com>
2023-11-04 21:59:15 +0100

    db_tables

--- src/Python/pypower.py
+++ src/Python/pypower.py
@@ -4,7 +4,7 @@ import pandas as pd
 import seaborn as sns
 
 
-
+# DATA CLEANING
 def check_nan(df: pd.DataFrame) -> None:
     
     """ Función de Yona
@@ -35,6 +35,14 @@ def check_nan(df: pd.DataFrame) -> None:
 
 def column_unification(df):
 
+    """
+    Function that remove spaces from rows
+    and
+    lowercase and replace spaces with _
+    in the columns names
+    
+    """
+
     #Remove spaces from rows
     for col in df.select_dtypes(include='object'):
         df[col] = df[col].str.strip()
@@ -45,46 +53,12 @@ def column_unification(df):
     return df
 
 
-def column_to_list(column):
-    ls=[]
-    for e in column:
-        for s in str(e).split(','):
-            ls.append(s)
-
-    return ls
-
 def rename_columns(df, dictio):
     """
     Function to change the name of the columns specified in the dictio param
     
     """    
     return df.rename(columns=dictio)
-
-
-
-
-def split_string(string, character, wish_len, filler):
-
-    lst= string.split(character)
-    if(len(lst)<wish_len):
-        for i in range(wish_len-len(lst)):
-            lst.append(filler)
-    elif(len(lst)>wish_len):
-        for i in range(len(lst)-wish_len):
-            lst.pop()
-    
-    return lst 
-
-#So proud of this function :D
-def fill_column(row, df_to_compare, column_to_compare, column_to_change):
-    
-    if row[column_to_compare] in df_to_compare[column_to_compare].values:
-        
-        matching_row = df_to_compare[df_to_compare[column_to_compare] == row[column_to_compare]]
-        
-        row[column_to_change] = matching_row[column_to_change].values[0]
-
-    return row
 
 
 def replace_to(string, change_for, change_to):
@@ -101,6 +75,7 @@ def replace_to(string, change_for, change_to):
     return string.replace(change_for, change_to)
 
 
+
 def change_type(df, column, type_from, type_to):
 
     """
@@ -111,7 +86,6 @@ def change_type(df, column, type_from, type_to):
     return the modified DataFrame
     
     """
-
 
     #If the type from is object
     if type_from == 'object':
@@ -129,7 +103,6 @@ def change_type(df, column, type_from, type_to):
                 df[column] = df[column].astype(int)
 
     
-
     #If the type from is float
     elif type_from == 'float':
 
@@ -146,7 +119,6 @@ def change_type(df, column, type_from, type_to):
                 df[column] = df[column].astype(int)
 
     
-
     #If the type from is int
     elif type_from == 'int64':
 
@@ -162,5 +134,58 @@ def change_type(df, column, type_from, type_to):
             elif type_to=='object':
                 df[column] = df[column].astype(object)
 
-
     return df
+
+
+
+# PROCESSING DATAFRAMES
+
+def new_table(df_from, column_to_check, create_id, id_column_name, column_name,  id_list=[]):
+
+    """
+    Function to create a new dataframe by creating a list of unique values 
+    from df_from[column_to_check] and in case create_id is True, creates a 
+    list of consecutive values for id, otherwise assigns the ids from id_list
+    
+    """
+    #Read the values of the column_to_check
+    data=[]
+
+    for e in df_from[column_to_check]:
+        if e not in data:
+            data.append(e)
+    
+
+    #If create_id then create a list of consecutive values for the id
+    if create_id:
+        ids=[]
+
+        for e in range(len(data)):
+            ids.append(str(e+1))
+
+    elif not create_id:
+        ids= id_list
+
+    #Create a dictionary with both lists
+    dict_temp= {id_column_name: ids, column_name: data}
+
+    return pd.DataFrame(dict_temp)
+
+
+
+#So proud of this function :D
+def fill_column(row, df_to_compare, column_to_compare, column_to_change):
+
+    """
+    
+    
+    """
+    
+    if row[column_to_compare] in df_to_compare[column_to_compare].values:
+        
+        matching_row = df_to_compare[df_to_compare[column_to_compare] == row[column_to_compare]]
+        
+        row[column_to_change] = matching_row[column_to_change].values[0]
+
+    return row
+
